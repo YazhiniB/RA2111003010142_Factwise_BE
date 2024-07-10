4738af6eceba6dbdb611f14b13f101df894821ff
YazhiniB <yv5429@srmist.edu.in>
2024-07-10 14:35:29 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Accordion from "./Accordion";
+import CelebritiesArray from "./celebrities.json";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [celebrities, setCelebrities] = useState(CelebritiesArray);
+  const updateUser = (update, edit, setEdit) => {
+    let userArray = celebrities;
+    let objIndex = celebrities.findIndex((obj) => obj.id == update.id);
+    userArray[objIndex] = update;
+    setCelebrities([...userArray]);
+    setEdit(0);
+  };
+  const deleteUser = (deleteUser) => {
+    let userArray = celebrities;
+    userArray = userArray.filter(function (user) {
+      return user.id !== deleteUser.id;
+    });
+    setCelebrities([...userArray]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row justify-content-center my-3">
+          <div className="col-8">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search User"
+            />
+          </div>
+        </div>
+        <div className="accordion" id="accordionExample">
+          {celebrities.map((celeb) => (
+            <Accordion
+              celeb={celeb}
+              key={celeb.id}
+              updateUser={updateUser}
+              deleteUser={deleteUser}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
